3615398734edb5134a77ea6aa973b75448354cb2
VuLinh <vnlinh51@gmail.com>
2022-05-24 20:18:47 +0700

    install antd

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -1,9 +1,31 @@
+import { useState } from 'react';
 import styles from './app.module.css';
+import 'antd/dist/antd.css';
+import { Col, Divider, Row } from 'antd';
 
 export function App() {
+  const [check, setCheck] = useState(false);
   return (
     <>
-      <div className='flex text-red-500'>check</div>
+      <div className="flex text-red-500">
+        <h1>Check!</h1>
+      </div>
+
+      <Divider orientation="left">Normal</Divider>
+      <Row>
+        <Col span={6} order={4}>
+          1 col-order-4
+        </Col>
+        <Col span={6} order={3}>
+          2 col-order-3
+        </Col>
+        <Col span={6} order={2}>
+          3 col-order-2
+        </Col>
+        <Col span={6} order={1}>
+          4 col-order-1
+        </Col>
+      </Row>
     </>
   );
 }
